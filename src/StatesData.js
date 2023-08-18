fd88e2aa586dbe118ffba3b12cb59a782c4c8f03
DinnuBunny <dineshkandula007@gmail.com>
2023-08-18 14:15:57 +0000

    Added Maps Feature to Each State

---
+++ src/StatesData.js
@@ -0,0 +1,223 @@
+const IndianStatesList = [
+  {
+    state_code: 'AN',
+    state_name: 'Andaman and Nicobar Islands',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361465/Covid19_Dashboard/Header/States/AN_ngcooz.svg',
+  },
+  {
+    state_code: 'AP',
+    state_name: 'Andhra Pradesh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692363285/Covid19_Dashboard/Header/States/AP_ssgfoy.svg',
+  },
+  {
+    state_code: 'AR',
+    state_name: 'Arunachal Pradesh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361471/Covid19_Dashboard/Header/States/AR_zos4mr.svg',
+  },
+  {
+    state_code: 'AS',
+    state_name: 'Assam',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361465/Covid19_Dashboard/Header/States/AS_lyxdpb.svg',
+  },
+  {
+    state_code: 'BR',
+    state_name: 'Bihar',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361473/Covid19_Dashboard/Header/States/BR_xjlckq.svg',
+  },
+  {
+    state_code: 'CH',
+    state_name: 'Chandigarh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361467/Covid19_Dashboard/Header/States/CH_voazcz.svg',
+  },
+  {
+    state_code: 'CT',
+    state_name: 'Chhattisgarh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361470/Covid19_Dashboard/Header/States/CT_zpdbru.svg',
+  },
+  {
+    state_code: 'DL',
+    state_name: 'Delhi',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361463/Covid19_Dashboard/Header/States/DL_hnfi13.svg',
+  },
+  {
+    state_code: 'DN',
+    state_name: 'Dadra and Nagar Haveli and Daman and Diu',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361463/Covid19_Dashboard/Header/States/DN_nixuaj.svg',
+  },
+
+  {
+    state_code: 'GA',
+    state_name: 'Goa',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361472/Covid19_Dashboard/Header/States/GA_jujcvb.svg',
+  },
+  {
+    state_code: 'GJ',
+    state_name: 'Gujarat',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361473/Covid19_Dashboard/Header/States/GJ_j0h3ev.svg',
+  },
+  {
+    state_code: 'HP',
+    state_name: 'Himachal Pradesh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361465/Covid19_Dashboard/Header/States/HP_ciftif.svg',
+  },
+  {
+    state_code: 'HR',
+    state_name: 'Haryana',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361467/Covid19_Dashboard/Header/States/HR_pvjshs.svg',
+  },
+  {
+    state_code: 'JH',
+    state_name: 'Jharkhand',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361470/Covid19_Dashboard/Header/States/JH_bq4k6u.svg',
+  },
+  {
+    state_code: 'JK',
+    state_name: 'Jammu and Kashmir',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361462/Covid19_Dashboard/Header/States/JK_ywquyp.svg',
+  },
+
+  {
+    state_code: 'KA',
+    state_name: 'Karnataka',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361467/Covid19_Dashboard/Header/States/KA_uigdzf.svg',
+  },
+  {
+    state_code: 'KL',
+    state_name: 'Kerala',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361469/Covid19_Dashboard/Header/States/KL_piqhoe.svg',
+  },
+  {
+    state_code: 'LA',
+    state_name: 'Ladakh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361463/Covid19_Dashboard/Header/States/LA_dme27y.svg',
+  },
+  {
+    state_code: 'LD',
+    state_name: 'Lakshadweep',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361467/Covid19_Dashboard/Header/States/LD_kpzapc.svg',
+  },
+  {
+    state_code: 'MH',
+    state_name: 'Maharashtra',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361469/Covid19_Dashboard/Header/States/MH_fcsvaa.svg',
+  },
+  {
+    state_code: 'MP',
+    state_name: 'Madhya Pradesh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361474/Covid19_Dashboard/Header/States/MP_ahxjx3.svg',
+  },
+  {
+    state_code: 'ML',
+    state_name: 'Meghalaya',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361472/Covid19_Dashboard/Header/States/ML_sc4lgx.svg',
+  },
+  {
+    state_code: 'MN',
+    state_name: 'Manipur',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361473/Covid19_Dashboard/Header/States/MN_cezfkj.svg',
+  },
+
+  {
+    state_code: 'MZ',
+    state_name: 'Mizoram',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361473/Covid19_Dashboard/Header/States/MZ_kdf6rs.svg',
+  },
+  {
+    state_code: 'NL',
+    state_name: 'Nagaland',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361472/Covid19_Dashboard/Header/States/NL_ysazww.svg',
+  },
+  {
+    state_code: 'OR',
+    state_name: 'Odisha',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361469/Covid19_Dashboard/Header/States/OR_vfmvku.svg',
+  },
+  {
+    state_code: 'PB',
+    state_name: 'Punjab',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361465/Covid19_Dashboard/Header/States/PB_fzerw4.svg',
+  },
+  {
+    state_code: 'PY',
+    state_name: 'Puducherry',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361463/Covid19_Dashboard/Header/States/PY_t4qu1j.svg',
+  },
+  {
+    state_code: 'RJ',
+    state_name: 'Rajasthan',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361465/Covid19_Dashboard/Header/States/RJ_s3tttb.svg',
+  },
+  {
+    state_code: 'SK',
+    state_name: 'Sikkim',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361471/Covid19_Dashboard/Header/States/SK_efsq11.svg',
+  },
+  {
+    state_code: 'TG',
+    state_name: 'Telangana',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361467/Covid19_Dashboard/Header/States/TN_rwmwy0.svg',
+  },
+  {
+    state_code: 'TN',
+    state_name: 'Tamil Nadu',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361467/Covid19_Dashboard/Header/States/TN_rwmwy0.svg',
+  },
+  {
+    state_code: 'TR',
+    state_name: 'Tripura',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361469/Covid19_Dashboard/Header/States/TR_zlbxzn.svg',
+  },
+  {
+    state_code: 'UP',
+    state_name: 'Uttar Pradesh',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361465/Covid19_Dashboard/Header/States/UP_ic0twr.svg',
+  },
+  {
+    state_code: 'UT',
+    state_name: 'Uttarakhand',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361463/Covid19_Dashboard/Header/States/UT_uwsdie.svg',
+  },
+  {
+    state_code: 'WB',
+    state_name: 'West Bengal',
+    imageUrl:
+      'https://res.cloudinary.com/dinnubunny/image/upload/v1692361471/Covid19_Dashboard/Header/States/WB_hept5o.svg',
+  },
+]
+
+export default IndianStatesList
--- src/components/StateSpecific/index.js
+++ src/components/StateSpecific/index.js
@@ -8,154 +8,10 @@ import CovidMetaState from '../CovidMetaState'
 import TopDistricts from '../TopDistricts'
 import ReChartBar from '../ReChartBar'
 import ReChartLine from '../ReChartLine'
-import './index.css'
 
-const IndianStatesList = [
-  {
-    state_code: 'AN',
-    state_name: 'Andaman and Nicobar Islands',
-  },
-  {
-    state_code: 'AP',
-    state_name: 'Andhra Pradesh',
-  },
-  {
-    state_code: 'AR',
-    state_name: 'Arunachal Pradesh',
-  },
-  {
-    state_code: 'AS',
-    state_name: 'Assam',
-  },
-  {
-    state_code: 'BR',
-    state_name: 'Bihar',
-  },
-  {
-    state_code: 'CH',
-    state_name: 'Chandigarh',
-  },
-  {
-    state_code: 'CT',
-    state_name: 'Chhattisgarh',
-  },
-  {
-    state_code: 'DN',
-    state_name: 'Dadra and Nagar Haveli and Daman and Diu',
-  },
-  {
-    state_code: 'DL',
-    state_name: 'Delhi',
-  },
-  {
-    state_code: 'GA',
-    state_name: 'Goa',
-  },
-  {
-    state_code: 'GJ',
-    state_name: 'Gujarat',
-  },
-  {
-    state_code: 'HR',
-    state_name: 'Haryana',
-  },
-  {
-    state_code: 'HP',
-    state_name: 'Himachal Pradesh',
-  },
-  {
-    state_code: 'JK',
-    state_name: 'Jammu and Kashmir',
-  },
-  {
-    state_code: 'JH',
-    state_name: 'Jharkhand',
-  },
-  {
-    state_code: 'KA',
-    state_name: 'Karnataka',
-  },
-  {
-    state_code: 'KL',
-    state_name: 'Kerala',
-  },
-  {
-    state_code: 'LA',
-    state_name: 'Ladakh',
-  },
-  {
-    state_code: 'LD',
-    state_name: 'Lakshadweep',
-  },
-  {
-    state_code: 'MH',
-    state_name: 'Maharashtra',
-  },
-  {
-    state_code: 'MP',
-    state_name: 'Madhya Pradesh',
-  },
-  {
-    state_code: 'MN',
-    state_name: 'Manipur',
-  },
-  {
-    state_code: 'ML',
-    state_name: 'Meghalaya',
-  },
-  {
-    state_code: 'MZ',
-    state_name: 'Mizoram',
-  },
-  {
-    state_code: 'NL',
-    state_name: 'Nagaland',
-  },
-  {
-    state_code: 'OR',
-    state_name: 'Odisha',
-  },
-  {
-    state_code: 'PY',
-    state_name: 'Puducherry',
-  },
-  {
-    state_code: 'PB',
-    state_name: 'Punjab',
-  },
-  {
-    state_code: 'RJ',
-    state_name: 'Rajasthan',
-  },
-  {
-    state_code: 'SK',
-    state_name: 'Sikkim',
-  },
-  {
-    state_code: 'TN',
-    state_name: 'Tamil Nadu',
-  },
-  {
-    state_code: 'TG',
-    state_name: 'Telangana',
-  },
-  {
-    state_code: 'TR',
-    state_name: 'Tripura',
-  },
-  {
-    state_code: 'UP',
-    state_name: 'Uttar Pradesh',
-  },
-  {
-    state_code: 'UT',
-    state_name: 'Uttarakhand',
-  },
-  {
-    state_code: 'WB',
-    state_name: 'West Bengal',
-  },
-]
+import IndianStatesList from '../../StatesData'
+
+import './index.css'
 
 const apiConst = {
   success: 'SUCCESS',
@@ -223,17 +79,24 @@ class StateSpecific extends Component {
   )
 
   renderSuccessView = () => {
+    window.scrollTo(0, 0)
     const {stateCovidData, stateTimelinesData, activeMeta} = this.state
     const {match} = this.props
     const {stateCode} = match.params
-    const stateName = IndianStatesList.find(
+    const index = IndianStatesList.findIndex(
       each => each.state_code === stateCode,
-    ).state_name
+    )
 
+    const stateName = IndianStatesList[index].state_name
+    const {imageUrl} = IndianStatesList[index]
+
+    const {population} = stateCovidData.meta
     const lastUpdated = stateCovidData.meta.last_updated
     const newDate = new Date(lastUpdated)
     const formattedDate = format(newDate, 'PPP')
+    const asPerFormattedDate = format(newDate, 'dd MMM')
     const {tested} = stateCovidData.total
+
     return (
       <div className="state-specific-container">
         <div className="name-last-update-tested-card">
@@ -247,6 +110,25 @@ class StateSpecific extends Component {
               <br />
               <span className="tested-count">{tested}</span>
             </p>
+          </div>
+        </div>
+        <div className="map-population-tested">
+          <img src={imageUrl} alt={stateName} className="map-image" />
+          <div className="population-tested">
+            <h1 className="map-ncp-report">NCP report</h1>
+            <div className="population-testes-container">
+              <div className="title-count-container">
+                <p className="map-title">Population</p>
+                <p className="map-count">{population}</p>
+              </div>
+              <div>
+                <p className="map-title">Tested</p>
+                <p className="map-count">{tested}</p>
+                <p className="as-of-date">
+                  (As of {asPerFormattedDate} per source)
+                </p>
+              </div>
+            </div>
           </div>
         </div>
         <CovidMetaState
